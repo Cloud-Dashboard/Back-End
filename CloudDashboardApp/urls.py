1cd1d0fe7e7224f1daab27231cb68b8ee8838bf8
rauluis <LRS18110328@purisima.tecnm.mx>
2022-10-12 19:36:22 -0700

    first commit

---
+++ CloudDashboardApp/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path
+from CloudDashboardApp.views import  home,about,dashboard,data,lista
+from CloudDashboardApp.views import data13Widgets,data4TalbesB,index,data12UiTyp,data11UiBut,data10UiTabs,data9IuSwi,data8UiP,data7UiModals,data3TablesD,datauiAlerts,data2formsB,data1ChartsF,data0ChartsC
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', home, name="home"),
+    path('about', about, name="about"),
+    path('dashboard', dashboard, name="dashboard"),
+    path('data', data, name="data"),
+    path('lista',lista,name="lista"),
+    path('data13Widgets',data13Widgets,name='data13Widgets'),
+    path('data12UiTyp',data12UiTyp,name='data12UiTyp'),
+    path('data11UiBut',data11UiBut,name='data11UiBut'),
+    path('index',index,name='index'),
+    path('data4TalbesB',data4TalbesB,name='data4TalbesB'),
+    path('data10UiTabs',data10UiTabs,name='data10UiTabs'),
+    path('data9IuSwi',data9IuSwi,name='data9IuSwi'),
+    path('data8UiP',data8UiP,name='data8UiP'),
+    path('data7UiModals',data7UiModals,name='data7UiModals'),
+    path('data3TablesD',data3TablesD,name='data3TablesD'),
+    path('datauiAlerts',datauiAlerts,name='datauiAlerts'),
+    path('data2formsB',data2formsB,name='data2formsB'),
+    path('data1ChartsF',data1ChartsF,name='data1ChartsF'),
+    path('data0ChartsC',data0ChartsC,name='data0ChartsC'),
+    
+]
+
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
---
+++ CloudDashboardApp/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render, HttpResponse
+
+
+from CloudDashboardApp.models import Document
+from CloudDashboardApp.forms import DocumentForm
+
+def lista(request):
+    # Handle file upload
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            newdoc = Document(docfile = request.FILES['docfile'])
+            newdoc.save()
+
+            # Redirect to the document list after POST
+    else:
+        form = DocumentForm() # A empty, unbound form
+
+    # Load documents for the list page
+    documents = Document.objects.all()
+
+    # Render list page with the documents and the form
+    return render(request,'CloudDashboardApp/list.html',{'documents': documents, 'form': form})
+
+    
+# Create your views here.
+def about(request):
+    return render(request, "CloudDashboardApp/about.html")
+def home(request):
+    return render(request, "CloudDashboardApp/home.html")
+def dashboard(request):
+    return render(request, "CloudDashboardApp/dashboard.html")  
+def data(request):
+    return render(request, "CloudDashboardApp/data.html")
+
+def upload(request):
+    return render(request,"upload.html")
+
+def data0ChartsC(request):
+    return render(request, "CloudDashboardApp/charts-chartjs.html")
+def data1ChartsF(request):
+    return render(request, "CloudDashboardApp/charts-flot.html")
+def data2formsB(request):
+    return render(request, "CloudDashboardApp/forms-basic.html")
+def index(request):
+    return render(request, "CloudDashboardApp/index.html")
+def data4TalbesB(request):
+    return render(request, "CloudDashboardApp/tables-basic.html")
+def data3TablesD(request):
+    return render(request, "CloudDashboardApp/tables-data.html")
+def datauiAlerts(request):
+    return render(request, "CloudDashboardApp/ui-alerts.html")
+def data7UiModals(request):
+    return render(request, "CloudDashboardApp/ui-modals.html")
+def data8UiP(request):
+    return render(request, "CloudDashboardApp/ui-progressbar.html")
+def data9IuSwi(request):
+    return render(request, "CloudDashboardApp/ui-switches.html")
+def data10UiTabs(request):
+    return render(request, "CloudDashboardApp/ui-tabs.html")
+def data11UiBut(request):
+    return render(request, "CloudDashboardApp/ui-buttons.html")
+def data12UiTyp(request):
+    return render(request, "CloudDashboardApp/ui-typgraphy.html")
+def data13Widgets(request):
+    return render(request, "CloudDashboardApp/widgets.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
